18a14e0e7759e8a1846ab6c3214dbc37f81ff46f
Raed Bahri <38087431+lord007tn@users.noreply.github.com>
2026-04-30 05:44:23 +0100

    ci: configure changelogithub sections

---
+++ changelogithub.config.mjs
@@ -0,0 +1,22 @@
+export default {
+  types: {
+    feat: { title: "🚀 Features" },
+    fix: { title: "🐞 Bug Fixes" },
+    perf: { title: "🏎 Performance" },
+    refactor: { title: "💅 Refactors" },
+    docs: { title: "📖 Documentation" },
+    design: { title: "🎨 Design" },
+    style: { title: "🎨 Styles" },
+    ci: { title: "🤖 CI" },
+    build: { title: "📦 Build" },
+    test: { title: "✅ Tests" },
+    chore: { title: "🏡 Chore" },
+  },
+  titles: {
+    breakingChanges: "🚨 Breaking Changes",
+  },
+  contributors: true,
+  emoji: true,
+  capitalize: true,
+  group: true,
+};
